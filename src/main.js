9351e70b6a194ce3c88addeb09a3b9283339cbb3
jack <jackzhoumine@gmail.com>
2022-06-05 02:59:44 +0800

    feat: tokens 替换

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import * as dom from './lib/dom.js'
 import dayjs from 'dayjs'
 
 // get clock element
-const clock = dom.get('.clock')
+const clock = dom.get('__clock_selector')
+
+console.log('aB_e')
 
 if (clock) {
   console.log('initializing clock...')
 
   // update clock every second
   setInterval(() => {
-    clock.textContent = dayjs().format('HH:mm:ss')
-  }, 1000)
+    clock.textContent = dayjs().format('__time_format')
+  }, __clock_interval)
 }
