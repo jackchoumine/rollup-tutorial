cfe7e7f81ffbb99770ee140107858746b7c8f6b5
jack <jackzhoumine@gmail.com>
2022-06-05 09:17:18 +0800

    feat: 插件

--- rollup.config.all.js
+++ rollup.config.all.js
@@ -2,7 +2,7 @@
  * @Description : 打包输出CJS
  * @Date        : 2021-12-30 01:17:13 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-06-05 03:31:37 +0800
+ * @LastEditTime: 2022-06-05 07:56:08 +0800
  * @LastEditors : JackChou
  */
 
@@ -11,7 +11,16 @@ import typescript from '@rollup/plugin-typescript'
 const formats = [
   { format: 'cjs' },
   // NOTE iife、umd 需要提供导出的名字
-  { format: 'iife', name: 'hello' },
+  {
+    format: 'iife',
+    name: 'hello',
+    //NOTE 给打包输出注入注释
+    banner: '/* library version 5 */',
+    // TODO
+    // globals: {
+    //   jquery: '@',
+    // },
+  },
   { format: 'umd', name: 'hello' },
   { format: 'system' },
   { format: 'esm' },
